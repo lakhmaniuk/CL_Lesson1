d3033d5534bd921cf83fb726898259e857c7c96c
Mykhailo Lakhmaniuk <lmiha@nm.ru>
2016-02-06 14:15:34 +0200

    Update

--- app/src/main/java/com/codaline/lakhmaniuk/lesson1/MainActivity.java
+++ app/src/main/java/com/codaline/lakhmaniuk/lesson1/MainActivity.java
@@ -5,10 +5,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     Button btnNarnia;
+    EditText textToNarnia;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,10 +19,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.main);
 
         btnNarnia = (Button) findViewById(R.id.btn_Narnia);
+        textToNarnia = (EditText)findViewById(R.id.toNarniaText);
+
     }
 
     public void goToNarnia(View view){
+
         Intent intent = new Intent(this,NarniaActivity.class );
-        startActivity(intent);
+        intent.putExtra("send message", textToNarnia.getText().toString());
+        startActivityForResult(intent, 1);;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (data == null) {return;}
+        String name = data.getStringExtra("send message");
+        Toast.makeText(this, name, Toast.LENGTH_SHORT).show();
     }
 }
--- app/src/main/java/com/codaline/lakhmaniuk/lesson1/NarniaActivity.java
+++ app/src/main/java/com/codaline/lakhmaniuk/lesson1/NarniaActivity.java
@@ -8,24 +8,32 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class NarniaActivity extends AppCompatActivity {
 
     Button backTTRW;
+    EditText textFromNarnia;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.narnia_activity);
 
-
         backTTRW = (Button) findViewById(R.id.btn_Back);
+        textFromNarnia = (EditText)findViewById(R.id.fromNarniaText);
+        Intent intent = getIntent();
+        String inMessage = intent.getStringExtra("send message");
+        Toast.makeText(this,inMessage, Toast.LENGTH_SHORT).show();
 
 
     }
 
     public void backTTRW(View view){
         Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("send message", textFromNarnia.getText().toString());
+        setResult(RESULT_OK, intent);
         finish();
 
     }
